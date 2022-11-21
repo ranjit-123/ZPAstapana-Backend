e95309443fe704c7131e7be3b676f3ac124b9edf
Ranjit Gange <ranjitgange@gmail.com>
2022-11-21 11:35:46 +0530

    origin changes

--- src/main/java/com/zpasthapana/ZpasthapanaApplication.java
+++ src/main/java/com/zpasthapana/ZpasthapanaApplication.java
@@ -21,7 +21,7 @@ public class ZpasthapanaApplication {
 	   return new WebMvcConfigurerAdapter() {
 	      @Override
 	      public void addCorsMappings(CorsRegistry registry) {
-	         registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+	         registry.addMapping("/**").allowedOrigins("*");
 	      }    
 	   };
 	}
