2047a4e9b996621152e1b57bb444a1bc3a2164e2
Ranjit Gange <ranjitgange@gmail.com>
2022-11-21 11:32:46 +0530

    origin issue changes

--- src/main/java/com/zpasthapana/ZpasthapanaApplication.java
+++ src/main/java/com/zpasthapana/ZpasthapanaApplication.java
@@ -2,7 +2,11 @@ package com.zpasthapana;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableScheduling
@@ -12,4 +16,13 @@ public class ZpasthapanaApplication {
 		SpringApplication.run(ZpasthapanaApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+	   return new WebMvcConfigurerAdapter() {
+	      @Override
+	      public void addCorsMappings(CorsRegistry registry) {
+	         registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+	      }    
+	   };
+	}
 }
