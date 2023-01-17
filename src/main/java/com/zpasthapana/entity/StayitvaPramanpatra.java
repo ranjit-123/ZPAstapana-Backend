a73bb1645edc6b2eb15c461d2dbda427086e9200
Mahesh <maheshthorat55@gmail.com>
2023-01-18 01:17:51 +0530

    New changes

--- src/main/java/com/zpasthapana/entity/StayitvaPramanpatra.java
+++ src/main/java/com/zpasthapana/entity/StayitvaPramanpatra.java
@@ -10,8 +10,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
-import org.springframework.web.multipart.MultipartFile;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
