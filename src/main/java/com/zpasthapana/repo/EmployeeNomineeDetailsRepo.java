06107c57100e278f760f21ce7d46f182fb7fc5b4
Guruprasad Patil <gurupatil2421@gmail.com>
2024-11-05 15:40:13 +0530

    Added findByEmployeeId()

--- src/main/java/com/zpasthapana/repo/EmployeeNomineeDetailsRepo.java
+++ src/main/java/com/zpasthapana/repo/EmployeeNomineeDetailsRepo.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.zpasthapana.entity.EmployeeNomineeDetails;
 
-public interface EmployeeNomineeDetailsRepo extends JpaRepository<EmployeeNomineeDetails, Long>{
+public interface EmployeeNomineeDetailsRepo extends JpaRepository<EmployeeNomineeDetails, Long> {
+
+	EmployeeNomineeDetails findByEmployeeId(Long employeeId);
 
 }
